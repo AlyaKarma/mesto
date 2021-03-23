c26907b75f7a974876ab66064df7fc2bdbe47e76
Alya Karma <amakamma.karma@gmail.com>
2021-03-23 20:58:52 +0300

    fix: внести правки после ревью_2

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
 let popup = document.querySelector('.popup');
 let popupOpenBtn = document.querySelector('.profile__open-btn');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
-let popupOverlay = document.querySelector('.popup__overlay');
 let popupName = document.querySelector('.popup__input_type_name');
 let popupProfession = document.querySelector('.popup__input_type_profession')
 let nameProfile = document.querySelector('.profile__name');
 let professionProfile = document.querySelector('.profile__profession');
+let popupSave =document.querySelector('.popup__save');
 
 function openPopup() {
   popup.classList.add('popup_visible');
@@ -19,7 +19,10 @@ function formSubmitHandler(evt) {
   evt.preventDefault();
   nameProfile.textContent = popupName.value;
   professionProfile.textContent = popupProfession.value;
+  closePopup();
 }
+
+popupSave.addEventListener('click', formSubmitHandler);
 
 popupOpenBtn.addEventListener('click', function() {
   popupName.value = nameProfile.textContent;
