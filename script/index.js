71061d54f40bbe5a763d65aecbe0ea459587b5cc
Alya Karma <amakamma.karma@gmail.com>
2021-03-23 16:15:14 +0300

    fix: внести правки после ревью_1

--- script/index.js
+++ script/index.js
@@ -1,9 +1,9 @@
 let popup = document.querySelector('.popup');
-let popupOpenBtn = document.getElementById('popup_open-btn');
+let popupOpenBtn = document.querySelector('.popup_open-btn');
 let popupCloseBtn = document.querySelector('.popup__close-btn');
 let popupOverlay = document.querySelector('.popup__overlay');
-let popupName = document.querySelector('.popup__name');
-let popupProfession = document.querySelector('.popup__profession')
+let popupName = document.querySelector('.popup__input_type_name');
+let popupProfession = document.querySelector('.popup__input_type_profession')
 let nameProfile = document.querySelector('.profile__name');
 let professionProfile = document.querySelector('.profile__profession');
 
@@ -15,22 +15,16 @@ function closePopup() {
   popup.classList.remove('popup_visible');
 }
 
-popupOpenBtn.addEventListener('click', function(event) {
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = popupName.value;
+  professionProfile.textContent = popupProfession.value;
+}
+
+popupOpenBtn.addEventListener('click', function() {
   popupName.value = nameProfile.textContent;
   popupProfession.value = professionProfile.textContent;
   openPopup();
 })
 
-
-popupCloseBtn.addEventListener('click', function() {
-  nameProfile.textContent = popupName.value;
-  professionProfile.textContent = popupProfession.value;
-  closePopup();
-});
-
-popupOverlay.addEventListener('click', function() { closePopup(); });
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-
-}
+popupCloseBtn.addEventListener('click', closePopup);
